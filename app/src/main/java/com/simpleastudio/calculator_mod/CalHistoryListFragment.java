5faac57fc3afc5b0c5b68adcec5498269bc23dee
JonnyLee <jonnylee719@gmail.com>
2015-10-01 22:42:04 -0300

    Set up clear history button and back button: - clear history button uses calHistory to clear history - back button returns to calview activity with result as RESULT_CANCELED

--- app/src/main/java/com/simpleastudio/calculator_mod/CalHistoryListFragment.java
+++ app/src/main/java/com/simpleastudio/calculator_mod/CalHistoryListFragment.java
@@ -1,14 +1,14 @@
 package com.simpleastudio.calculator_mod;
 
-import android.content.Context;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.support.v4.view.LayoutInflaterFactory;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,7 +19,9 @@ import java.util.ArrayList;
 public class CalHistoryListFragment extends ListFragment {
     private static final String TAG = "CalHistoryListFragment";
     private ArrayList<Equation> mEquations;
-    private EquationAdaptor mAdapter;
+    private EquationAdapter mAdapter;
+    private Button mClearHistoryButton;
+    private Button mBackButton;
 
     public CalHistoryListFragment(){
 
@@ -30,7 +32,7 @@ public class CalHistoryListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         mEquations = CalHistory.get(getActivity()).getmEquations();
 
-        mAdapter = new EquationAdaptor(mEquations);
+        mAdapter = new EquationAdapter(mEquations);
         setListAdapter(mAdapter);
     }
 
@@ -39,12 +41,30 @@ public class CalHistoryListFragment extends ListFragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_list_calhistory, container, false);
 
+        mClearHistoryButton = (Button) v.findViewById(R.id.button_clear_history);
+        mClearHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalHistory.get(getActivity()).clearCalHistory();
+                ((EquationAdapter)getListAdapter()).notifyDataSetChanged();
+            }
+        });
+        mBackButton = (Button) v.findViewById(R.id.button_back);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                getActivity().setResult(Activity.RESULT_CANCELED, returnIntent);
+                getActivity().finish();
+            }
+        });
+
         return v;
     }
 
-    private class EquationAdaptor extends ArrayAdapter<Equation>{
+    private class EquationAdapter extends ArrayAdapter<Equation>{
 
-        public EquationAdaptor(ArrayList<Equation> equations) {
+        public EquationAdapter(ArrayList<Equation> equations) {
             super(getActivity(), 0, equations);
         }
 
